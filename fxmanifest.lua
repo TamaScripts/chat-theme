14ee24292b73f5725aa850c7a06d61f2135e0203
Magnus <64981416+MMagnusss@users.noreply.github.com>
2024-08-01 03:30:40 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version "cerulean"
+game "gta5"
+author "Tama Scripts"
+version "1.0.0"
+
+file "style.css"
+
+chat_theme "tama" {
+  styleSheet = "style.css"
+  -- msgTemplates = { -- Add your own if you want
+  --   default = "<b>{0}</b><span>{1}</span>"
+  -- }
+}
+
